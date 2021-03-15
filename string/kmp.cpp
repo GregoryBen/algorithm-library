5e6bde888256f1bd6876067f3fc03e5de566ae8c
GregoryBen <59790640+GregoryBen@users.noreply.github.com>
2021-03-15 16:31:15 +0700

    Update kmp.cpp

--- string/kmp.cpp
+++ string/kmp.cpp
@@ -4,7 +4,7 @@ template <typename T>
 vector<int> prefix_function(const T &s) {
   int n = (int) s.size(), j = 0;
   vector<int> p(n);
-  for(int i = 1; i < n; i++) {
+  for (int i = 1; i < n; i++) {
     while (j > 0 && s[i] != s[j]) {
       j = p[j - 1];
     }
